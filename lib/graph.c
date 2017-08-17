b4ac2b3bd5511ebd233e6e4472eba697c737c377
Josh Abraham <sinisterpatrician@gmail.com>
2017-08-17 15:55:37 -0400

    Add initializer to graph program

--- lib/graph.c
+++ lib/graph.c
@@ -1,13 +1,37 @@
 #include "graph.h"
 
+static double dist(double th1, double ph1, double th2, double ph2)
+{   /* https://rosettacode.org/wiki/Haversine_formula#C */
+#define R 6371
+#define TO_RAD (3.1415926536 / 180)
+    double dx, dy, dz;
+    ph1 -= ph2;
+    ph1 *= TO_RAD, th1 *= TO_RAD, th2 *= TO_RAD;
+
+    dz = sin(th1) - sin(th2);
+    dx = cos(ph1) * cos(th1) - cos(th2);
+    dy = sin(ph1) * cos(th1);
+    return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * R;
+}
+
 Graph_t *mkgraph(int vertices)
 {
     Graph_t *g = malloc(sizeof(Graph_t));
-    g->vert = vertices;
+    g->verts = vertices;
     g->mat = malloc(vertices * sizeof(int*));
 
     for (int i = 0; i < vertices; i++)
         g->mat[i] = malloc(vertices * sizeof(int));
 
     return g;
+}
+
+void initgraph(Graph_t *g)
+{
+    for (int i = 0; i < g->verts; i++)
+        for (int j = 0; j < g->verts; j++)
+            g->mat[i][j] = 0;
+    //TODO: determine adjacency here
+
+    return;
 }
--- lib/graph.h
+++ lib/graph.h
@@ -5,12 +5,13 @@
 // GRAPH[i][j] = 0 if i and j are NOT adjacent
 
 typedef struct _graph {
-    int vert;
+    int verts;
     int edge;
     int **mat; //2-d matrix
 } Graph_t;
 
 Graph_t *mkgraph(int vertices);
 void rmgraph(Graph_t *g);
+void initgraph(Graph_t *g);
 
 #endif
